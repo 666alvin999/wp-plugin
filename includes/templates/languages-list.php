9d7ed6e72b8c5d4b75a545c55c6508d315f3dbb4
VidarDev <>
2024-02-28 16:15:41 +0100

    style: :lipstick: ajout des styles CSS + Correction de l'HTML

--- includes/templates/languages-list.php
+++ includes/templates/languages-list.php
@@ -10,31 +10,27 @@
 
 	if ($query->have_posts()) : ?>
 
-		<div id="languages">
+		<div id="languages" class="programming-languages-container">
 			<?php while ($query->have_posts()):
 				$query->the_post();
 				?>
 
-				<div class="language-card">
-					<figure>
-						<?= wp_get_attachment_image(get_field("programming_language_logo"), 'full', '', array('class' => 'language-logo')) ?>
-					</figure>
+				<div class="programming-languages-card">
+					<figure class="programming-languages-card-img">
+                        <img src="<?= get_field("programming_language_logo")['url'] ?>" alt="<?= get_field("programming_language_logo")['alt'] ?>">
+                    </figure>
 
-					<h3 class="language-name">
+					<h3 class="programming-languages-card-title">
 						<?= get_field("programming_language_name") ?>
 					</h3>
 
-					<div class="language-description">
-						<h4 class="language-discription-title"></h4>
-
+					<div class="programming-languages-card-description">
 						<p><?= get_field("programming_language_description") ?></p>
 					</div>
 
-					<div class="language-website">
-						<a href="<?= get_field("programming_language_website") ?>">
-							Go to website
-						</a>
-					</div>
+                    <a class="programming-languages-card-button" href="<?= get_field("programming_language_website") ?>">
+                        Go to website
+                    </a>
 				</div>
 
 			<?php endwhile; ?>
