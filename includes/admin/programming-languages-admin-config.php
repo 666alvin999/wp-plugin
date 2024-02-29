f8452fb19955fcaf105777b9bd13e675a2f11c77
Alvin Hamaïde <alvin98mc@gmail.com>
2024-02-29 10:25:07 +0100

    update

--- includes/admin/programming-languages-admin-config.php
+++ includes/admin/programming-languages-admin-config.php
@@ -42,6 +42,34 @@ class ProgrammingLanguagesAdmin {
         );
 
         register_post_type(self::POST_TYPE, $args);
+
+	    $labels = array(
+		    'name' => __('PL Carousel', 'Post Type General Name'),
+		    'singular_name' => __('PL Carousel', 'Post Type Singular Name'),
+		    'menu_name' => __('PL Carousel'),
+		    'all_items' => __('All Carousel'),
+		    'view_item' => __('View Carousel'),
+		    'add_new_item' => __('Add a new Carousel'),
+		    'add_new' => __('Add'),
+		    'edit_item' => __('Edit Carousel'),
+		    'update_item' => __('Update Carousel'),
+		    'search_items' => __('Search for a Carousel'),
+		    'not_found' => __('Carousel not found'),
+		    'not_found_in_trash' => __('Carousel not found in trash')
+	    );
+
+	    $args = array(
+		    'label' => __('Carousel Turn Off-On'),
+		    'description' => __('Turn your programming languages in carousel'),
+		    'labels' => $labels,
+		    'menu_icon' => 'dashicons-html',
+		    'supports' => array('title', 'editor', 'thumbnail', 'true_false', 'author'),
+		    'show_in_rest' => true,
+		    'hierarchical' => false,
+		    'public' => true,
+		    'has_archive' => true,
+		    'rewrite' => array('slug' => 'programminglanguages'),
+	    );
     }
 
 }
